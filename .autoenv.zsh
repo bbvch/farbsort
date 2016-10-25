681811259b09f90269745a02f5266c2960adf571
goerks <michael.burkard@gmx.ch>
2016-10-25 10:20:28 +0200

    added command to copy image to sd card

--- .autoenv.zsh
+++ .autoenv.zsh
@@ -1,1 +1,3 @@
-autostash alias enter-env='source sources/poky/oe-init-build-env build'
+autostash alias activate='source sources/poky/oe-init-build-env build'
+autostash alias copy-image='sudo dd bs=4M if=$(ls /var/tmp/wic/build/sdimage-bootpart-*.direct) of=/dev/mmcblk0'
+
